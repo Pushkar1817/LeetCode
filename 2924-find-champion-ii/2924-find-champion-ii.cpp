64bac105416e0cdf495a20463c175c96a848f7ce
Pushkar1817 <157231667+Pushkar1817@users.noreply.github.com>
2024-11-26 13:18:56 +0530

    Time: 12 ms (67.29%), Space: 99.5 MB (59.14%) - LeetHub

---
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>revAdj(n);
+        for(int i = 0 ;i< edges.size() ;i++){
+            revAdj[edges[i][1]].push_back(edges[i][0]);
+        }
+        int ans = -1;
+        for(int i = 0 ;i< n; i++){
+            if(!revAdj[i].size()){
+                if(ans!=-1)
+                    return -1;
+                ans = i;
+            }
+        }
+        return ans ;
+    }
+};
