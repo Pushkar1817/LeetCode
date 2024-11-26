9f4ad010191cab7f6b781d39c3b9228da3589b7b
Pushkar1817 <157231667+Pushkar1817@users.noreply.github.com>
2024-11-26 13:29:37 +0530

    Time: 4 ms (72.43%), Space: 93.4 MB (71.57%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.cpp
+++ 2924-find-champion-ii/2924-find-champion-ii.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>revAdj(n);
-        for(int i = 0 ;i< edges.size() ;i++){
-            revAdj[edges[i][1]].push_back(edges[i][0]);
+        // vector<vector<int>>revAdj(n);
+        // for(int i = 0 ;i< edges.size() ;i++){
+        //     revAdj[edges[i][1]].push_back(edges[i][0]);
+        // }
+        // int ans = -1;
+        // for(int i = 0 ;i< n; i++){
+        //     if(!revAdj[i].size()){
+        //         if(ans!=-1)
+        //             return -1;
+        //         ans = i;
+        //     }
+        // }
+        // return ans ;
+        vector<bool>weaker(n,false);
+        for(int i = 0 ; i<edges.size() ;i++){
+            weaker[edges[i][1]] = true;
         }
         int ans = -1;
-        for(int i = 0 ;i< n; i++){
-            if(!revAdj[i].size()){
-                if(ans!=-1)
+        for(int i = 0 ;i < n ; i++){
+            if(weaker[i] == false){
+                if(ans != -1)  
                     return -1;
                 ans = i;
             }
